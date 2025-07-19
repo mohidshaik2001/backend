91447bf24fd67be76306afb3a9840a7c542ee025
Shaik Khaja Mohid <mohidshaik2001@gmail.com>
2025-07-19 23:21:32 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -23,4 +23,11 @@ app.use(
 app.use(express.static("public"));
 
 app.use(cookieParser());
+
+//routes import
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
