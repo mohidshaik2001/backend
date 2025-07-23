8d334820b191683821735c9bbffb1dcdfac21389
Shaik Khaja Mohid <mohidshaik2001@gmail.com>
2025-07-23 18:55:48 +0530

    Fix current-user route method from POST to GET

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -32,7 +32,7 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
 router
